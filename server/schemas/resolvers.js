eb2bf134317314a7772576bca410ddd04a7c9e35
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-07-17 15:18:30 -0500

    resolvers and typeDefs added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,58 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id})
+                    .select('-__v -password')
+                    .populate('savedBooks');
+
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+        saveBook: async (parent, {input}, context) => {
+            if (context.user) {
+                const userData = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: input}},
+                    {new: true}
+                );
+                return userData;
+            }
+            throw new AuthenticationError('Something went wrong');
+        },
+        removeBook: async (parent, {bookId}, context) => {
+            if (context.user) {
+                const userData = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId}}},
+                    {new: true}
+                );
+                return userData;
+            }
+            throw new AuthenticationError('Something went wrong');
+        }
+    }
+};
+
+module.exports = resolvers;
